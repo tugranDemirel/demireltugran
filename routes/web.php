84d1e477c0404cc4926eeaadc5e309a6d62c982f
tugranDemirel <demireltugran66@gmail.com>
2022-03-12 22:37:54 +0300

    auth işlemleri tamamlandı. Route üzerinde düzenlenme yapıldı

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,5 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/admin', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
